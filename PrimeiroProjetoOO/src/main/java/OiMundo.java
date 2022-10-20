f06aaf991b9321318d7f895671d25b1e6876804d
Sergio Herman <sergioherman@id.uff.br>
2022-10-20 15:34:25 -0300

    Primeiro projeto desenvolvido.

--- PrimeiroProjetoOO/src/main/java/OiMundo.java
+++ PrimeiroProjetoOO/src/main/java/OiMundo.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,5 +13,13 @@
 public class OiMundo {
     public static void main(String[] args){
         System.out.println("Oi MUndo");
+        Scanner teclado = new Scanner(System.in);
+        int idade;
+        String nome;
+        System.out.println("Digite a sua idade: ");
+        idade = teclado.nextInt();
+        System.out.println("Digite o seu nome: ");
+        nome = teclado.next();
+        System.out.println("Nome: " + nome + "\nIdade: " + idade);
     }
 }
