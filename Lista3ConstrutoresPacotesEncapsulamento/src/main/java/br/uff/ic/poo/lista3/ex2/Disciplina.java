7ecd84fcc25200a5c6a0ee4ed57162b331349b3d
Sergio Herman <sergioherman@id.uff.br>
2022-10-24 12:59:17 -0300

    Iniciado ex2 da lista 2.

---
+++ Lista3ConstrutoresPacotesEncapsulamento/src/main/java/br/uff/ic/poo/lista3/ex2/Disciplina.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.uff.ic.poo.lista3.ex2;
+
+/**
+ *
+ * @author aluno
+ */
+public class Disciplina {
+    
+}
